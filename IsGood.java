8d2acda393c3d6ab22412817cc781c758dcd73ba
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2022-12-26 19:00:24 +0300

    Создал интерфейс и использовал его в проверке на четность

---
+++ IsGood.java
@@ -0,0 +1,4 @@
+public interface IsGood<T> {
+    
+    public boolean isGood(T item);
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+public static void main(String[] args) {
+ IsEven even = new IsEven();
+ System.out.println(even.isGood(10));   
+}
+    
+}
